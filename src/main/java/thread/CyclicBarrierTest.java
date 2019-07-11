0724348e6d0772b5a85b4edf942fa9440981281a
fengwei <fengwei@thunisoft.com>
2019-07-11 17:32:33 +0800

    修改cyclicBarrier代码

--- src/main/java/thread/CyclicBarrierTest.java
+++ src/main/java/thread/CyclicBarrierTest.java
@@ -3,51 +3,61 @@ package thread;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * cyclicBarrier.await();次数到了配置值后往下走；
+ * new CyclicBarrier（count,runable）runable的执行线程是最后一个触发await（）并达到次数的线程
+ */
 public class CyclicBarrierTest {
-	public final CyclicBarrier cyclicBarrier;
-	public int count;
-	public final ConcurrentLinkedQueue<String> linkedQueue = new ConcurrentLinkedQueue<String>();
-	public final ConcurrentLinkedQueue<Integer> orderQueue = new ConcurrentLinkedQueue<Integer>();
+    public final CyclicBarrier cyclicBarrier;
+    public int count;
+    public final ConcurrentLinkedQueue<String> linkedQueue = new ConcurrentLinkedQueue<String>();
+    public final ConcurrentLinkedQueue<Integer> orderQueue = new ConcurrentLinkedQueue<Integer>();
+    private Integer times = 2;
+    private Integer currentTimes = 1;
+    private AtomicInteger order = new AtomicInteger(0);
 
-	public CyclicBarrierTest(int count) {
-		this.count = count;
-		this.cyclicBarrier = new CyclicBarrier(count, new Runnable() {
-			public void run() {
-				System.out.println(linkedQueue.toString());
-			}
-		});
-		for (int i = 0; i < count; i++) {
-			orderQueue.add(i);
-		}
-	}
+    public CyclicBarrierTest(int count) {
+        this.count = count;
+        this.cyclicBarrier = new CyclicBarrier(count, new Runnable() {
+            public void run() {
+                String threadName = Thread.currentThread().getName();
+                System.out.println("thread-" + threadName);
+                if (++currentTimes <= times) {
+                    start();
+                    System.out.println("current:" + currentTimes);
+                }
+            }
+        });
+    }
 
-	public void start() {
-		for (int i = 0; i < count; i++) {
-			new Thread() {
-				public void run() {
-					int i = orderQueue.poll();
-					linkedQueue.add(i + "");
-					try {
-						sleep(3000l);
-						System.out.println(i + " is watting...");
-						cyclicBarrier.await();
-						System.out.println(i + " is wake up");
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					} catch (BrokenBarrierException e) {
-						e.printStackTrace();
-					}
-				}
-			}.start();
-		}
-	}
+    public void start() {
+        for (int i = 0; i < count; i++) {
+            new Thread() {
+                public void run() {
+                    String threadName = Thread.currentThread().getName();
+                    try {
+                        sleep(5000L);
+                        int i = order.addAndGet(1);
+                        System.out.println(threadName + "-" + i + " is watting...");
+                        cyclicBarrier.await();
+                        System.out.println(threadName + "-" + i + " is wake up");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (BrokenBarrierException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+        }
+    }
 
-	public class Worker {
+    public class Worker {
 
-	}
+    }
 
-	public static void main(String[] args) {
-		new CyclicBarrierTest(5).start();
-	}
+    public static void main(String[] args) {
+        new CyclicBarrierTest(5).start();
+    }
 }
